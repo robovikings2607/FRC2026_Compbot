e22a44442187abd9b17270dfe8d129cea3073f66
consultfitz13 <consultfitz13@gmail.com>
2026-02-16 15:47:30 -0500

    fix some bugs with units

--- src/main/java/frc/robot/subsystems/TurretSubsystemExp.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystemExp.java
@@ -5,10 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -16,9 +13,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
-import frc.robot.Constants.HoodConstants;
 import frc.robot.Constants.TurretConstants;
 import frc.robot.utilities.GeometryUtil;
 
@@ -102,7 +97,7 @@ public class TurretSubsystemExp extends ShooterComponentSubsystemExp {
       Translation2d targetCoordinates, 
       ChassisSpeeds robotVelocity) {
     
-      final double SHOT_VELOCITY_MPS = 10.0; // Speed of ball leaving shooter (Tune this!)
+      final double SHOT_VELOCITY_MPS = 8.0; // Speed of ball leaving shooter (Tune this!)
 
       Pose2d turretPose = GeometryUtil.getOffsetPose(
         robotPose,
@@ -152,7 +147,8 @@ public class TurretSubsystemExp extends ShooterComponentSubsystemExp {
       // --- 2. THE REALITY (Actual Turret State) ---
       // Get the current position and velocity from the motor.
       // Convert rotations to degrees if your motor API uses rotations.
-      double actualDegrees = motor.getPosition().getValueAsDouble() * 360.0;
+      double actualDegrees = GeometryUtil.getMotorRotationsAsDegrees(
+        motor.getPosition().getValueAsDouble(), GEAR_RATIO);
 
       // --- 3. THE ERROR (Distance to Truth) ---
       // Use inputModulus to handle the -180/180 wrap correctly.
@@ -192,9 +188,9 @@ public class TurretSubsystemExp extends ShooterComponentSubsystemExp {
         slot0Configs.kS = 0.25; // Voltage output to overcome static friction
         slot0Configs.kV = 0.12; // A velocity target of 1 rps requires this voltage output.
         slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires this voltage output
-        slot0Configs.kP = 4.8; // A position error of 2.5 rotations requires this voltage output
+        slot0Configs.kP = 20.0; // A position error of .55 rotations requires this voltage output
         slot0Configs.kI = 0; // no output for integrated error
-        slot0Configs.kD = 0.11; // A velocity error of 1 rps requires this voltage output
+        slot0Configs.kD = 0.5; // A velocity error of .5 rps requires this voltage output
 
     var motionMagicConfigs = configs.MotionMagic;
         motionMagicConfigs.MotionMagicCruiseVelocity = 160; // Target cruise velocity of 80 rps
@@ -209,8 +205,9 @@ public class TurretSubsystemExp extends ShooterComponentSubsystemExp {
 
     // 2. SET THE THRESHOLDS (in Rotations)
     // ALWAYS set these slightly safer than the physical hard stop.
-    configs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = rotationsPerDegree * MAX_TURRET_ANGLE_DEGREES;
-    configs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = rotationsPerDegree * MIN_TURRET_ANGLE_DEGREES;
+    configs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = GeometryUtil.getMotorRotationsAsDegrees(MAX_TURRET_ANGLE_DEGREES, GEAR_RATIO);
+    configs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = GeometryUtil.getMotorRotationsAsDegrees(MIN_TURRET_ANGLE_DEGREES, GEAR_RATIO);;
+
 
     motor.getConfigurator().apply(configs);
     motor.setNeutralMode(NeutralModeValue.Brake);
